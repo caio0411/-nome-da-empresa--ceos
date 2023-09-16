7bf9e19ea056d0f500f937011a7c08cd134779f4
Joelio-Pinheiro <joeliopinheiro10@gmail.com>
2023-09-16 14:28:57 -0300

    Adicionando a navbar

---
+++ src/app/components/navbar/NavBar.js
@@ -0,0 +1,15 @@
+import styles from './navbar.module.css'
+export default function NavBar() {
+    return (
+        <header className={styles.navbar}>
+            <div className={styles.logo}>Agência</div>
+            <container className={styles.menu}>
+                <div><a href='#'>Início</a></div>
+                <div><a href='#'>O que fazemos</a></div>
+                <div><a href='#'>Equipe</a></div>
+                <div><a href='#'>Sobre nós</a></div>
+            </container>
+            <button className={styles.faleconosco}><a href='#'>Fale Conosco</a></button>
+        </header>
+    );
+}
